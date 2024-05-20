555297158a94171b3c2427e17ace678e9a1de282
Adnan <adnanmasih8@gmail.com>
2024-05-21 02:49:12 +0500

    Class 7

---
+++ Class 7/app.js
@@ -0,0 +1,5 @@
+var num1 = 7; // statement  hard coded hai
+var num2 = 5; // statement
+var sum = num1 + num2; //  expression  dynamic  value ha
+// + is operator and 7, 5 are operant
+console.log(sum);
---
+++ Class 7/app.ts
@@ -0,0 +1,15 @@
+let num1 = 7;      // statement  hard coded hai
+let num2 = 5;      // statement
+let sum = num1 + num2;      //  expression  dynamic  value ha
+// + is operator and 7, 5 are operant
+console.log(sum)
+// same as - * / %    
+
+//  ** exponentiation is square
+let res = num1 ** num2;  //  exp  5**3 = 125      5**2=25
+
+// % modulus is remainder ju bach jae      percentage ek mukamal formula ha yeh modulus ha remaining
+let res2 = 5%2 //   11%3 = 2   9%3=0 
+
+// unary operator   uni mean 1     
+// pre  pehle ++a          post bad main a++     pre main (update huna phir  value return)
